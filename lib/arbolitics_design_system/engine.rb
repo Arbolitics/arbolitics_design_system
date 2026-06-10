1b21eb6f9e1c2b55e77aa474585d0ad468ed7d15
IvanRiveraN <ivan@arbolitics.com>
2026-06-09 21:31:29 -0400

    uopdated engine to expose lucide rails

--- lib/arbolitics_design_system/engine.rb
+++ lib/arbolitics_design_system/engine.rb
@@ -3,5 +3,11 @@
 module ArboliticsDesignSystem
   class Engine < ::Rails::Engine
     isolate_namespace ArboliticsDesignSystem
+
+    initializer "arbolitics_design_system.view_helpers" do
+      ActiveSupport.on_load(:action_view) do
+        include LucideRails::RailsHelper
+      end
+    end
   end
 end
